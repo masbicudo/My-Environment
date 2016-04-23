7e39ef0264b9e105406791bb738c98c2ddb646a2
masbicudo <masbicudo@gmail.com>
2016-04-23 14:42:08 -0300

    Setup file associations to my like

---
+++ WinEnvSetup/set-file-types.bat
@@ -0,0 +1,6 @@
+assoc .nuspec=NuGet.Specification
+ftype NuGet.Specification="C:\Program Files (x86)\Notepad++\notepad++.exe" %1
+
+FOR /F "usebackq tokens=1-2 delims==" %%L IN (
+    `assoc .zip`
+) DO assoc .nupkg=%%M
