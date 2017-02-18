d031a45430c4f89c98310fae8eae92a1cfdc6206
masbicudo <masbicudo@gmail.com>
2017-02-18 14:18:28 -0200

    My environment variables.

--- WinEnvSetup/setup-masb-environment.bat
+++ WinEnvSetup/setup-masb-environment.bat
@@ -8,7 +8,11 @@ CALL set-desktop-dir
 CALL append-system-env-var PATHEXT ".LNK"
 CALL append-user-env-var   PATH "%DESKTOPDIR%"
 CALL append-user-env-var   PATH "%__CURDIR83%\tools"
+CALL set-system-env-var    MyEnv "%__CURDIR83%" /M
 CALL append-user-env-var   PATH "%DESKTOPDIR%\Shortcuts"
 CALL append-user-env-var   SHORTCUTS "%DESKTOPDIR%\Shortcuts"
+CALL append-user-env-var   PROJECTS "C:\Projetos"
+CALL append-user-env-var   PATH "%PROJECTS%\CMD-scripts"
+CALL append-user-env-var   CLASSPATH "C:\Programas\JavaApps"
 
 SET __CURDIR83=
