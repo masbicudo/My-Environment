fe3ab05bc3eb9995a73a3d8ffb9aab65f3bc05e5
Miguel Angelo <masbicudo@gmail.com>
2017-05-11 23:51:22 -0300

    Allow en-US and pt-BR folder names as my environment folders.

--- WinEnvSetup/setup-masb-environment.bat
+++ WinEnvSetup/setup-masb-environment.bat
@@ -4,21 +4,27 @@ SET PATH=%PATH%;%cd%\tools
 
 for %%f in ("%cd%") do SET __CURDIR83=%%~sf
 
+CALL append-user-env-var   PATH "%__CURDIR83%\tools"
+CALL set-system-env-var    MyEnv "%__CURDIR83%" /M
+
 CALL set-desktop-dir
 CALL append-system-env-var PATHEXT ".LNK"
 CALL append-user-env-var   PATH "%DESKTOPDIR%"
-CALL append-user-env-var   PATH "%__CURDIR83%\tools"
-CALL set-system-env-var    MyEnv "%__CURDIR83%" /M
 CALL append-user-env-var   PATH "%DESKTOPDIR%\Shortcuts"
 CALL append-user-env-var   SHORTCUTS "%DESKTOPDIR%\Shortcuts"
-CALL append-user-env-var   PROJECTS "C:\Projetos"
-CALL append-user-env-var   PATH "%PROJECTS%\CMD-scripts"
-CALL append-user-env-var   PATH "C:\Programas\JavaApps"
-CALL append-user-env-var   CLASSPATH "."
-CALL append-user-env-var   CLASSPATH "C:\Programas\JavaApps\antlr4-complete.jar"
 
-CALL append-user-env-var   PATH "C:\Programas\GnuWin32\bin"
-CALL append-user-env-var   PATH "C:\Programas\UnxUtils\usr\local\wbin"
-CALL append-user-env-var   PATH "C:\Programas\UnxUtils\bin"
+IF EXIST "C:\Projetos\" CALL append-user-env-var   PROJECTS "C:\Projetos"
+IF EXIST "C:\Projects\" CALL append-user-env-var   PROJECTS "C:\Projects"
+CALL append-user-env-var   PATH "%PROJECTS%\CMD-scripts"
+
+IF EXIST "C:\Programas\" CALL append-user-env-var  PROGRAMS "C:\Programas"
+IF EXIST "C:\Programs\" CALL append-user-env-var   PROGRAMS "C:\Programs"
+CALL append-user-env-var   PATH "%PROGRAMS%\JavaApps"
+CALL append-user-env-var   CLASSPATH "."
+CALL append-user-env-var   CLASSPATH "%PROGRAMS%\JavaApps\antlr4-complete.jar"
+
+CALL append-user-env-var   PATH "%PROGRAMS%\GnuWin32\bin"
+CALL append-user-env-var   PATH "%PROGRAMS%\UnxUtils\usr\local\wbin"
+CALL append-user-env-var   PATH "%PROGRAMS%\UnxUtils\bin"
 
 SET __CURDIR83=
